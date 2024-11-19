7b392af1fa6c50599bec8f109f846302e025d4f4
shorya <shoryajainstedwards@gmail.com>
2024-11-19 16:14:06 +0530

    Refactor : Update Add Product Form UI

--- src/app/dashboard/add-product/page.tsx
+++ src/app/dashboard/add-product/page.tsx
@@ -1,23 +1,22 @@
-import AddProductForm from "@/components/dashboard-ui/AddProductForm"
-import { db } from "@/lib/db"
-import { auth } from "@clerk/nextjs"
-import { redirect } from "next/navigation"
-import isAdmin from "@/lib/authHelpers"
+import AddProductForm from "@/components/dashboard-ui/AddProductForm";
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import isAdmin from "@/lib/authHelpers";
 
 const page = async () => {
+  const { userId } = auth();
+  const isAuthorized = await isAdmin(userId);
+  if (!isAuthorized) redirect("/");
 
-    const { userId } = auth()
-    const isAuthorized = await isAdmin(userId)
-    if (!isAuthorized) redirect('/')
+  const categories = await db.category.findMany();
 
-    const categories = await db.category.findMany()
+  return (
+    <div className="min-h-screnn w-screen overflow-hidden px-5 mt-5">
+      <AddProductForm categories={categories} />
+    </div>
+  );
+};
 
-    return (
-        <div className="min-h-screnn w-screen overflow-hidden px-5 mt-5">
-            <h1 className="font-nunito font-semibold text-3xl">Add Product</h1>
-            <AddProductForm categories={categories} />
-        </div>
-    )
-}
+export default page;
 
-export default page
